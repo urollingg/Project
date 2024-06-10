49e33c1b0411e659797fd2fa399b771b5f89800b
urollingg <161699185+urollingg@users.noreply.github.com>
2024-06-10 09:09:42 +0700

    Update New Text Document.sql

--- New Text Document.sql
+++ New Text Document.sql
@@ -1,1 +1,5 @@
-
+-- The database schema includes columns for movie types and titles, countries, release years, ratings, etc.
+### Exploring data
+SELECT type, COUNT(type)
+FROM netflix_titles
+GROUP BY type;
