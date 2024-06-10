833264ffc2a67b90958581b03cac87c0bac53a91
urollingg <161699185+urollingg@users.noreply.github.com>
2024-06-10 09:10:06 +0700

    Update New Text Document.sql

--- New Text Document.sql
+++ New Text Document.sql
@@ -1,5 +1,11 @@
--- The database schema includes columns for movie types and titles, countries, release years, ratings, etc.
+The database schema includes columns for movie types and titles, countries, release years, ratings, etc.
+
 ### Exploring data
+
+```sql
 SELECT type, COUNT(type)
 FROM netflix_titles
 GROUP BY type;
+```
+
+The dataset comprises a total of 8,807 rows, with 6,131 movies and 2,676 TV shows.
