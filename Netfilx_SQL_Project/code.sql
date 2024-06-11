f3a6ac1e4bccd2cb4fe1893379cf5628bbba70c6
urollingg <161699185+urollingg@users.noreply.github.com>
2024-06-11 12:38:10 +0700

    Delete Netfilx_SQL_Project/code.sql

--- Netfilx_SQL_Project/code.sql
+++
@@ -1,7 +0,0 @@
--- The database schema includes columns for movie types and titles, countries, release years, ratings, etc.
-# Exploring data
-```sql
-SELECT type, COUNT(type)
-FROM netflix_titles
-GROUP BY type;
-```
